33d09bf47439734eb85e32446e6c4d4135cccf46
Michelle Thomas <mlayerthomas@yahoo.com>
2020-07-27 19:45:32 -0400

    initialized map to plot points

--- README.md
+++ README.md
@@ -6,3 +6,6 @@ please do not try to use this on internet explorer
 I used a fetch command to get data from the wheretheiss.at API
 I found the fields that I wanted, parced the external data and displayed them to the webpage by manipulaing the DOM
 I created an Update button to hit to show the most updated coordinates for the ISS
+
+Load project and must hit the UPDATE button for data to display in real time
+
--- script.js
+++ script.js
@@ -4,18 +4,12 @@ const url = 'https://api.wheretheiss.at/v1/satellites/25544';
 async function getData() {
     const response = await fetch(url);
     const data = await response.json();
-    const { latitude, longitude, visibility, timestamp, name, id } = data;
+    const { latitude, longitude, visibility, timestamp, name, id, altitude } = data;
     document.getElementById('lat').textContent = latitude;
     document.getElementById('lon').textContent = longitude;
     document.getElementById('vis').textContent = visibility;
     document.getElementById('time').textContent = timestamp;
     document.getElementById('name').textContent = name;
     document.getElementById('id').textContent = id;
-}
-
-function status(res) {
-    if (!res.ok) {
-        throw new Error(res.statusText);
-    }
-    return res;
+    document.getElementById('alt').textContent = altitude;
 }
