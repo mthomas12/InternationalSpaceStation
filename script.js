d285a261e5a22644c6ec03ed6fe66183b07cc84f
Michelle Thomas <mlayerthomas@yahoo.com>
2020-07-29 11:21:29 -0400

    added map to plot lat and long points upon Update

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+const mymap = L.map('mapid').setView([0, 0], 1);
+//attribute needed to used open street map
+const attribution = '&copy; <a "href=https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
+//url for tiles from open street map
+const tileUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+const tiles = L.tileLayer(tileUrl, { attribution }).addTo(mymap);
+
+
+//url for ISS API
 const url = 'https://api.wheretheiss.at/v1/satellites/25544';
 
 // getData function
@@ -5,6 +14,9 @@ async function getData() {
     const response = await fetch(url);
     const data = await response.json();
     const { latitude, longitude, visibility, timestamp, name, id, altitude } = data;
+
+    L.marker([latitude, longitude]).addTo(mymap);
+
     document.getElementById('lat').textContent = latitude;
     document.getElementById('lon').textContent = longitude;
     document.getElementById('vis').textContent = visibility;
